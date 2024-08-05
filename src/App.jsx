a05472109ee762303f24e039492a762e21178313
scarqin <1054139596@qq.com>
2024-08-05 23:32:30 +0800

    feat: play music

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,48 @@ import 'aframe-event-set-component'
 import 'aframe-layout-component'
 import 'aframe-template-component'
 import 'aframe-proxy-event-component'
+import { useEffect } from 'react'
+
+const songInfo = {}
 function App() {
+  const getLink = async () => {
+    const res = await fetch('https://mockapi.eolink.com/bMtTxnl025f64ed8a398ee6996df94322d93712abb6e586/getmusic')
+    return await res.json()
+  }
+  const init = async () => {
+    const result = await getLink()
+    //TODO 多歌逻辑
+    const songData = result.data.songs[0]
+
+    // 替换封面图
+    document.getElementById('cover-image').setAttribute('src', songData.coverImage)
+    document.getElementById('audio').setAttribute('src', songData.audioUrl)
+    // 通过 backgrounds 定时更新背景图片
+    document.getElementById('city').setAttribute('src', songData.backgrounds[0].imageUrl)
+
+    const audio = document.getElementById('audio')
+    const changeBackground = () => {
+      const currentTime = audio.currentTime
+      console.log(currentTime)
+      const background = songData.backgrounds.find((bg) => currentTime >= bg.time)
+      if (background) {
+        document.getElementById('city').setAttribute('src', background.imageUrl)
+      }
+    }
+    audio.addEventListener('timeupdate', changeBackground)
+  }
+  useEffect(() => {
+    init()
+  }, [])
   return (
     <>
       <a-scene>
-        <a-entity
-          class="link"
-          geometry="primitive: plane; height: 1; width: 1"
-          material="shader: flat; src: ${thumb}"
-          event-set__mouseenter="scale: 1.2 1.2 1"
-          event-set__mouseleave="scale: 1 1 1"
-          event-set__click="_target: #image-360; _delay: 300; material.src: ${src}"
-          proxy-event="event: click; to: #image-360; as: fade"
-          sound="on: click; src: #click-sound"></a-entity>
         <a-assets>
-          {/* 全景图 */}
           <img id="city" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/city.jpg" />
-          <img id="city-thumb" src="/images/player.jpeg" />
-          <img id="sechelt-thumb" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-sechelt.jpg" />
-          <audio id="click-sound" src="https://cdn.aframe.io/360-image-gallery-boilerplate/audio/click.ogg" />
-          <img id="cubes" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/cubes.jpg" />
-          <img id="sechelt" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/sechelt.jpg" />
+          {/* 全景图 */}
+          <img id="cover-image" src="/images/music01.jpg" />
+
+          <audio id="audio" src="https://cdn.aframe.io/360-image-gallery-boilerplate/audio/click.ogg" />
         </a-assets>
 
         <a-sky
@@ -33,10 +54,8 @@ function App() {
           src="#city"
           animation__fade="property: components.material.material.color; type: color; from: #FFF; to: #000; dur: 300; startEvents: fade"
           animation__fadeback="property: components.material.material.color; type: color; from: #000; to: #FFF; dur: 300; startEvents: animationcomplete__fade"></a-sky>
-
         <a-entity id="links" layout="type: line; margin: 1.5" position="0 -1 -4">
-          <a-entity template="src: #link" data-src="#city" data-thumb="#city-thumb"></a-entity>
-          <a-entity template="src: #link" data-src="#sechelt" data-thumb="#sechelt-thumb"></a-entity>
+          <a-entity template="src: #link" data-src="#city" data-thumb="#cover-image"></a-entity>
         </a-entity>
 
         <a-entity camera look-controls>
